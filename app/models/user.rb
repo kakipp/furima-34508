07c2fe7c2becbb0e2e905206faf8042b571aacae
kakipp <75791004+kakipp@users.noreply.github.com>
2021-03-01 17:19:49 +0900

    修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nick_name,presence: true
-         validates :birth_day,presence: true
-         
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
-           validates :first_name
-           validates :last_name
-         end
-       
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX
-       
-         with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/}do
-           validates :first_name_furigana
-           validates :last_name_furigana
-        end
+  with_options presence: true do
+    validates :nick_name
+    validates :birth_day
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
+    validates :first_name
+    validates :last_name
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/}do
+    validates :first_name_furigana
+    validates :last_name_furigana
+  end
 end
